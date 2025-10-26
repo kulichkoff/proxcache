c0c4ece5f9489991beeea5e6d72b9d501bbb6480
Daniel Kulichkov <danchick03@gmail.com>
2025-10-26 20:05:19 +0300

    ref: refactor memcache tests

--- internal/cacher/memory_cache_test.go
+++ internal/cacher/memory_cache_test.go
@@ -8,58 +8,50 @@ import (
 	"testing"
 )
 
-func TestMemCacheServicePersistsResponses(t *testing.T) {
-	service := NewMemoryCacheService()
+func Test_MemCacheService(t *testing.T) {
+	t.Run("Should have nil response in first", func(t *testing.T) {
+		service := NewMemoryCacheService()
+		req := httptest.NewRequest("GET", "localhost:8080/qqq", nil)
+		storedRes := service.GetResponse(req)
+		if storedRes != nil {
+			t.Errorf("Expected response to be nil first")
+		}
+	})
 
-	req := httptest.NewRequest("GET", "localhost:8080/qqq", nil)
-	res := &http.Response{
-		StatusCode: 404,
-		Status:     "404 Not found",
-	}
+	t.Run("Should persist response", func(t *testing.T) {
+		service := NewMemoryCacheService()
+		req := httptest.NewRequest("GET", "localhost:8080/qqq", nil)
+		res := &http.Response{
+			StatusCode: 404,
+			Status:     "404 Not found",
+		}
 
-	storedRes := service.GetResponse(req)
-	if storedRes != nil {
-		// Case 1: Should be nil before the save
-		t.Errorf("Expected response to be nil first")
-	}
+		service.SaveResponse(req, res)
+		storedRes := service.GetResponse(req)
 
-	service.SaveResponse(req, res)
-	storedRes = service.GetResponse(req)
+		if storedRes.StatusCode != 404 {
+			t.Errorf("Expected status code: %d; Got: %d", 404, storedRes.StatusCode)
+		}
+	})
 
-	if storedRes == nil {
-		// Case 2: Should not be nil after the save
-		t.Errorf("Expected response but got nil")
+	t.Run("Should persist response body", func(t *testing.T) {
+		testStr := "JSON CONtent here 22!!"
+		service := NewMemoryCacheService()
+		req := httptest.NewRequest("GET", "localhost:8080/qqq", nil)
+		res := &http.Response{
+			StatusCode: 200,
+			Body:       io.NopCloser(bytes.NewReader([]byte(testStr))),
+		}
 
-	} else if storedRes.StatusCode != 404 {
-		// Case 3: Should be the same as saved
-		t.Errorf("Expected code %d; Got %d", 404, storedRes.StatusCode)
-	}
-}
+		service.SaveResponse(req, res)
+		storedRes := service.GetResponse(req)
 
-func TestMemCacheServicePersistsResponseBody(t *testing.T) {
-	service := NewMemoryCacheService()
-
-	testStr := "JSON CONtent here 22!!"
-
-	req := httptest.NewRequest("GET", "localhost:8080/qqq", nil)
-	res := &http.Response{
-		StatusCode: 200,
-		Body:       io.NopCloser(bytes.NewReader([]byte(testStr))),
-	}
-
-	// TODO test original response body still accessible after save
-
-	service.SaveResponse(req, res)
-	storedRes := service.GetResponse(req)
-
-	if storedRes.Body == nil {
-		t.Errorf("Expected response body but got nil")
-	} else {
 		bodyBytes, _ := io.ReadAll(storedRes.Body)
 		bodyStr := string(bodyBytes)
-
 		if bodyStr != testStr {
 			t.Errorf("Expected content %s; Got %s", testStr, bodyStr)
 		}
-	}
+	})
+
+	// TODO test original response body still accessible after save
 }
